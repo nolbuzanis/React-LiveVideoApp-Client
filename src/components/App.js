380cffba7825513cebe59f81d3dc2a7f9170017c
nolbuzanis <nolanbuzanis@gmail.com>
2019-03-07 15:47:54 -0500

    Created Header component to be visible on all pages

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 // Stream components
 import StreamCreate from './streams/StreamCreate';
@@ -8,11 +8,14 @@ import StreamDelete from './streams/StreamDelete';
 import StreamShow from './streams/StreamShow';
 import StreamList from './streams/StreamList';
 
+import Header from './Header';
+
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <div>
+          <Header />
           <Route path='/' exact component={StreamList} />
           <Route path='/streams/new' exact component={StreamCreate} />
           <Route path='/streams/edit' exact component={StreamEdit} />
