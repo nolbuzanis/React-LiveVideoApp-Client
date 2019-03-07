d4b1a466403eb8decc1c1dea01f5c8dfdd788b43
nolbuzanis <nolanbuzanis@gmail.com>
2019-03-07 15:34:13 -0500

    Created stream components

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+const pageOne = () => {
+  return (
+    <div>
+      Page One
+      <Link to='/pagetwo'>Navigate to page2</Link>
+    </div>
+  );
+};
+
+const pageTwo = () => {
+  return (
+    <div>
+      Page Two
+      <Link to='/'>Navigate to page1</Link>
+    </div>
+  );
+};
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path='/' exact component={pageOne} />
+          <Route path='/pagetwo' component={pageTwo} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
