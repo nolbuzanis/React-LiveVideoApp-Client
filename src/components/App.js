ae03d597332710b988de762ad3f86c8d5f00f626
nolbuzanis <nolanbuzanis@gmail.com>
2019-03-07 15:38:34 -0500

    Used react router to wire up stream components on separate routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,23 @@
 import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 
-const pageOne = () => {
-  return (
-    <div>
-      Page One
-      <Link to='/pagetwo'>Navigate to page2</Link>
-    </div>
-  );
-};
-
-const pageTwo = () => {
-  return (
-    <div>
-      Page Two
-      <Link to='/'>Navigate to page1</Link>
-    </div>
-  );
-};
+// Stream components
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamShow from './streams/StreamShow';
+import StreamList from './streams/StreamList';
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <div>
-          <Route path='/' exact component={pageOne} />
-          <Route path='/pagetwo' component={pageTwo} />
+          <Route path='/' exact component={StreamList} />
+          <Route path='/streams/new' exact component={StreamCreate} />
+          <Route path='/streams/edit' exact component={StreamEdit} />
+          <Route path='/streams/delete' exact component={StreamDelete} />
+          <Route path='/streams/show' exact component={StreamShow} />
         </div>
       </BrowserRouter>
     </div>
