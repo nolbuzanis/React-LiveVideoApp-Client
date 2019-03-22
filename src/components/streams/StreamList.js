ce910286ffe4b880728d7c078f911f8aabf3f330
nolbuzanis <nolanbuzanis@gmail.com>
2019-03-22 14:54:19 -0400

    Added create stream button below stream list to show when user is logged in

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchStreams } from '../../actions/index';
+import { Link } from 'react-router-dom';
 
 class StreamList extends React.Component {
   componentDidMount() {
@@ -34,11 +35,24 @@ class StreamList extends React.Component {
     });
   };
 
+  renderCreate() {
+    if (this.props.isSignedIn) {
+      return (
+        <div style={{ textAlign: 'right' }}>
+          <Link to='/streams/new' className='ui button primary'>
+            Create Stream
+          </Link>
+        </div>
+      );
+    }
+  }
+
   render() {
     return (
       <div>
         <h2>streams</h2>
         <div className='ui celled list'>{this.renderList()}</div>
+        {this.renderCreate()}
       </div>
     );
   }
@@ -47,7 +61,8 @@ class StreamList extends React.Component {
 const mapStateToProps = state => {
   return {
     streams: Object.values(state.streams),
-    currentUserId: state.auth.userId
+    currentUserId: state.auth.userId,
+    isSignedIn: state.auth.isSignedIn
   }; //convert values from objct into elements of an array
 };
 
