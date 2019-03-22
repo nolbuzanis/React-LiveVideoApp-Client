d0bae11b99bda3479ad72239ae8f4ad3a8a08503
nolbuzanis <nolanbuzanis@gmail.com>
2019-03-22 02:19:02 -0400

    added all action creators for streams

--- src/actions/index.js
+++ src/actions/index.js
@@ -14,5 +14,41 @@ export const signOut = () => {
 };
 
 export const createStream = formValues => async dispatch => {
-  streams.post('/streams', formValues);
+  const response = await streams.post('/streams', formValues);
+  dispatch({
+    type: 'CREATE_STREAM',
+    payload: response.data
+  });
+};
+
+export const fetchStreams = () => async dispatch => {
+  const response = await streams.get('/streams');
+  dispatch({
+    type: 'FETCH_STREAMS',
+    payload: response.data
+  });
+};
+
+export const fetchStream = streamId => async dispatch => {
+  const response = await streams.get(`/streams/${streamId}`);
+  dispatch({
+    type: 'FETCH_STREAM',
+    payload: response.data
+  });
+};
+
+export const editStream = (streamId, formValues) => async dispatch => {
+  const response = await streams.put(`/streams/${streamId}`, formValues);
+  dispatch({
+    type: 'EDIT_STREAM',
+    payload: response.data
+  });
+};
+
+export const deleteStream = streamId => async dispatch => {
+  await streams.delete(`/streams/${streamId}`);
+  dispatch({
+    type: 'DELETE_STREAM',
+    payload: streamId
+  });
 };
