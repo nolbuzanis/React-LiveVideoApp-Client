1c34fd27369b42784a01ad027ff55978b34d0219
nolbuzanis <nolanbuzanis@gmail.com>
2019-03-22 19:36:42 -0400

    Wired up fetchStream action to StreamEdit component. Now stream edit can grab the stream associated with each individual url

--- src/components/App.js
+++ src/components/App.js
@@ -12,7 +12,6 @@ import StreamList from './streams/StreamList';
 import Header from './Header';
 
 const App = () => {
-  console.log(history);
   return (
     <div>
       <Router history={history}>
@@ -21,7 +20,7 @@ const App = () => {
           <div className='ui container'>
             <Route path='/' exact component={StreamList} />
             <Route path='/streams/new' exact component={StreamCreate} />
-            <Route path='/streams/edit' exact component={StreamEdit} />
+            <Route path='/streams/edit/:id' exact component={StreamEdit} />
             <Route path='/streams/delete' exact component={StreamDelete} />
             <Route path='/streams/show' exact component={StreamShow} />
           </div>
