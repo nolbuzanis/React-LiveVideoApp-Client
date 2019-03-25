8c03e3367f620b3a7e8a0949491bbbed0330e891
nolbuzanis <nolanbuzanis@gmail.com>
2019-03-24 23:33:34 -0400

    Styled Modal component and added basic event listeners for navigation

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import Modal from '../Modal';
+//import { fetchStream } from '../../actions/index';
 
-const StreamDelete = () => {
-  return <div>StreamDelete</div>;
-};
+class StreamDelete extends React.Component {
+  render() {
+    return (
+      <div>
+        StreamDelete <Modal />
+      </div>
+    );
+  }
+}
 
 export default StreamDelete;
