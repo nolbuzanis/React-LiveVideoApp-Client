57507de9faa8868a6277ec4b6e0e6c3ea231fffa
nolbuzanis <nolanbuzanis@gmail.com>
2019-03-27 16:36:02 -0400

    StreamShow displays the title and description of the selected stream

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -8,8 +8,17 @@ class StreamShow extends React.Component {
   }
 
   render() {
-    console.log(this.props);
-    return <div>StreamShow</div>;
+    if (!this.props.stream) {
+      return <div>Loading...</div>;
+    }
+
+    const { title, description } = this.props.stream;
+    return (
+      <div>
+        <h1>{title}</h1>
+        <h5>{description}</h5>
+      </div>
+    );
   }
 }
 
